ee0c6bfcb71f93e90a1f044e2337233c1f2e3b44
mseomun <andreww@designfever.com>
2025-09-08 21:37:55 +0900

    :bug: layout fix

--- dashboard/src/components/common/Footer.tsx
+++ dashboard/src/components/common/Footer.tsx
@@ -1,10 +1,15 @@
-import { Box, Divider, Grid } from '@mui/material';
+import { Box, Divider, Grid, Typography } from '@mui/material';
 
 export const Footer = () => {
   return (
     <Grid size={{ xs: 12 }}>
       <Box sx={{ mb: 5 }}>
         <Divider />
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', pt: 2 }}>
+          <Typography variant={'caption'}>
+            2025 📊 Demo dashboard for data visualization · MYEONGSU SEOMUN
+          </Typography>
+        </Box>
       </Box>
     </Grid>
   );
